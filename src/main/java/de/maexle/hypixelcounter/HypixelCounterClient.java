704658ccce1c559b34bdea0588ee1ada4917e605
Maexle4 <maxfm44@gmail.com>
2026-06-28 11:45:45 +0200

    Commit

---
+++ src/main/java/de/maexle/hypixelcounter/HypixelCounterClient.java
@@ -0,0 +1,11 @@
+package de.maexle.hypixelcounter;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HypixelCounterClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/de/maexle/hypixelcounter/HypixelCounterDataGenerator.java
@@ -0,0 +1,11 @@
+package de.maexle.hypixelcounter;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HypixelCounterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
